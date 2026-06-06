fc40c7affa6598f959a493098f196d2def175779
Akram Tsechoev <karabulaki@inbox.ru>
2026-06-06 20:54:36 +0300

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -10,63 +10,49 @@ import (
 func GetOrderHandler(w http.ResponseWriter, r *http.Request){
 
 		if r.Method != http.MethodGet{
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			w.WriteHeader(http.StatusMethodNotAllowed)	
-			json.NewEncoder(w).Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Метод не обнаружен"})
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Заказ не найден")
 			return
 		}
+		writeJSON(w, http.StatusOK, orders)
+		return
 
-		
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		// отправляет JSON
-		json.NewEncoder(w).Encode(orders)
-		
 	}
 
 func GetIdHandler(w http.ResponseWriter, r *http.Request){
 		id := r.PathValue("id") 
-
-	
 		if r.Method != http.MethodGet{
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			json.NewEncoder(w).Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Метод не обнаружен"})
+			writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не обнаружен" )
 			return 
 		}
 
 		idInt, err := strconv.Atoi(id)
 		if err != nil {
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(ErrorResp{Code: "INVALID_REQUEST", Message: "Некорректный запрос"})
+			
+			writeError(w, http.StatusBadRequest, "INVALID_REQUEST", "Некорректный запрос")
+		
 			return
 		}
 		
 		
 
 		for _, current := range orders {
+
+			// сравнение заказа с тем, что пришло в запросе
 			if current.ID == idInt {
-				w.Header().Set("Content-Type", "application/json; charset=utf-8")
-				json.NewEncoder(w).Encode(current)
+				writeJSON(w, http.StatusOK, current)
 				return
 			}
 		}
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusNotFound)
-
-		json.NewEncoder(w).Encode(ErrorResp{Code: "ORDER_NOT_FOUND", Message: "Заказ не найден"})
+		writeError(w, http.StatusNotFound, "ORDER_NOT_FOUND", "Заказ не найден")
 			return 
 	}
 
 func PostLoginHandler(w http.ResponseWriter, r *http.Request){	
 	if r.Method != http.MethodPost{
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		json.NewEncoder(w).Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Запрашиваемый адрес не поддерживает HTTP метод"})
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "HTTP-запрос не поддерживается")
 		return
 		}
 
-		
 		var login LoginRequest
 		
 		bodyLogin, err := io.ReadAll(r.Body)
@@ -76,110 +62,108 @@ func PostLoginHandler(w http.ResponseWriter, r *http.Request){
 		err = json.Unmarshal(bodyLogin, &login)
 
 		if login.Login != "client@example.com" || login.Password != "password"{
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(ErrorResp{Code: "INVALID_CREDENTIALS", Message: "Неправильный логин или пароль"})
+		writeError(w, http.StatusUnauthorized, "INVALID_CREDENTIALS", "Неправильный логин или пароль")
 		return
 		}
-	
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(map[string]string{"token": "fake-token"})
+		// if err != nil{
+		// 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		// 	w.WriteHeader(http.StatusBadRequest)
+		// 	json.NewEncoder(w).Encode(ErrorResp{Code: "VALIDATION_ERROR", Message: "Ошибка валидации"})
+		// 	//json.NewEncoder(w).Encode(map[string]string{"token": "fake-token"})
+		// 	return
+		// }
+
+		writeJSON(w, http.StatusOK, map[string]string{"token": "fake-token"})
 	}
-	
 
 
 func PostCommentHandler(w http.ResponseWriter, r *http.Request){
 		if r.Method != http.MethodPost {
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			json.NewEncoder(w).Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Метод не поддерживается"})
-			return
-		}
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
+		return
+	}
 	
 		
 		var comment Comment
 		defer r.Body.Close()
-		
 		bodyBytes, err := io.ReadAll(r.Body)
 		 
 		
 		err = json.Unmarshal(bodyBytes, &comment) 
 		if err != nil{
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(ErrorResp{Code: "VALIDATION_ERROR", Message: "Некорректный JSON"})
+			writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Некорректный JSON")
 			return
 		}
 
-		
+		// обращаюсь к полю Text структуры Comment
 		if comment.Text == ""{
-			json.NewEncoder(w).Encode(ErrorResp{Code: "VALIDATION_ERROR", Message: "Комментарий не должен быть пустым"})
+			writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Комментарий не должен быть пустым")
 			return
 		}
-		json.NewEncoder(w).Encode(SuccessResp{Code: "STATUS_200", Message: Операция прошла успешно!"})
-	}
+		writeJSON(w, http.StatusOK, map[string]string{"message": "comment sent"})
+}
+
 
 func GetProfileHandler(w http.ResponseWriter, r *http.Request){	
 	if r.Method != http.MethodGet{
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		json.NewEncoder(w).Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Метод не поддерживается"})
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
 		return
 		}
-
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(profile)
-}
-	
+		writeJSON(w, http.StatusOK, profile)
+	}
 
 func GetDocumentsHandler(w http.ResponseWriter, r *http.Request){
 	if r.Method != http.MethodGet{
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		json.NewEncoder(w). Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Статус не доступен"})
-		return 
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Статус не доступен")
+		return
 	}
 	id := r.PathValue("id")
 	id2, err := strconv.Atoi(id)
-	if err != nil {
+	if err != nil{
 	
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorResp{Code: "INVALID_REQUEST", Message: "Неверный запрос"})
-		return
-}
-	var res bool
-	for _, Order := range orders {
-
-		if Order.ID == id2 {
-			res = true
-			break
-		}
-	}
-	if res == false {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(ErrorResp{Code: "ORDER_NOT_FOUND", Message: "Заказ не найден"})
+		writeError(w, http.StatusBadRequest, "INVALID_REQUEST", "Неверный запрос")
 		return
 	}
+		// for _, doc := range documents{
+		// 	if doc.OrderID == id2{
+		// 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		// 		json.NewEncoder(w).Encode(documents)
+		// 	}
+			
+		//}
+		
+		var res bool
+		for _, Order := range orders{
+			
+			
+				if Order.ID == id2{
+					res = true
+					break
+				} 
+			}
+			if res == false {
+				writeError(w, http.StatusNotFound, "ORDER_NOT_FOUND", "Заказ не найден")
+				return 
+			}
 
-	var filtered []Document
-	for _, doc := range documents {
-		if doc.OrderID == id2 {
-			filtered = append(filtered, doc)
-		}
-	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(filtered)
-	return
+
+				
+			var filtered [] Document
+			for _, doc := range documents{
+				if doc.OrderID == id2{
+					filtered = append(filtered, doc)
+				}
+			}
+			writeJSON(w, http.StatusOK, filtered)
+			return
+			
 }
-	
 func PostLogoutHandler(w http.ResponseWriter, r *http.Request){
 	if r.Method != http.MethodPost{
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	json.NewEncoder(w). Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Метод не поддерживается"})
+	writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
 	return
 }
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(SuccessResp{Message: "Logged out"})
+	writeJSON(w, http.StatusOK, map[string]string{"text": "loggout"})
 
 }
 
@@ -187,11 +171,10 @@ func GetAuthHandler(w http.ResponseWriter, r *http.Request){
 	
 
 	if r.Method != http.MethodGet{
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		json.NewEncoder(w). Encode(ErrorResp{Code: "METHOD_NOT_ALLOWED", Message: "Метод не поддерживается"})
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
 		return
 	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	
 	user := Auth{Name: "Ivan", Email: "i_ivanov@test.com"}
-	json.NewEncoder(w).Encode(user)
+	writeJSON(w, http.StatusOK, user)
 }
