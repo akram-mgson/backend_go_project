d81d11f5b075aa49bf52f2287a982702640adea1
Akram Tsechoev <karabulaki@inbox.ru>
2026-06-07 15:54:54 +0300

    Create middleware.go

---
+++ middleware.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func AuthMiddleware(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader != "Bearer fake-token"{
+			writeError(w, http.StatusUnauthorized, "UNAUTHORIZED", "Неверный токен")
+			return
+		}
+			next.ServeHTTP(w,r)
+	
+			
+
+
+	})
+}
