65cb65a55d86175b6b81da167711c767e862c0b9
Akram Tsechoev <karabulaki@inbox.ru>
2026-06-10 22:50:48 +0300

    Update models.go

--- models.go
+++ models.go
@@ -33,7 +33,7 @@ type Document struct {
 	ID    int 		`json:"id"`
 	Title string	`json:"title"`
 	Body  string	`json:"body"`
-	OrderID int    `json:"orderid"`
+	OrderID int     `json:"order_id"`
 }
 
 
