ef0bc479d094ad00b594b5bc188768368461f710
Akram Tsechoev <karabulaki@inbox.ru>
2026-06-10 22:29:27 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
-func main(){
-	// функция, котор занимается обработкой эндпоинта
-
-
-	// иными словами - обработка запросов
-	http.HandleFunc("/api/cabinet/orders", GetOrderHandler)
-	http.HandleFunc("/api/cabinet/orders/{id}", GetIdHandler)
-	http.HandleFunc("/api/cabinet/auth/login", PostLoginHandler)
-	http.HandleFunc("/api/cabinet/orders/{id}/comment", PostCommentHandler)
-	http.HandleFunc("/api/cabinet/profile", GetProfileHandler)
-	http.HandleFunc("/api/cabinet/orders/{id}/documents", GetDocumentsHandler)
-	http.HandleFunc("/api/cabinet/aut/logout", PostLogoutHandler)
-	http.HandleFunc("/api/cabinet/auth/me", GetAuthHandler)
-
+func main() {
 
 	
+	http.HandleFunc("/api/cabinet/auth/login", PostLoginHandler)
+	http.HandleFunc("/api/cabinet/auth/logout", PostLogoutHandler)
+	http.HandleFunc("/api/cabinet/auth/me", GetAuthHandler)
+
+	// обертка handle
+	http.Handle("/api/cabinet/orders", AuthMiddleware(http.HandlerFunc(GetOrderHandler)))
+	http.Handle("/api/cabinet/orders/{id}", AuthMiddleware(http.HandlerFunc(GetIdHandler)))
+	http.Handle("/api/cabinet/orders/{id}/comment", AuthMiddleware(http.HandlerFunc(PostCommentHandler)))
+	http.Handle("/api/cabinet/profile", AuthMiddleware(http.HandlerFunc(GetProfileHandler)))
+	http.Handle("/api/cabinet/orders/{id}/documents", AuthMiddleware(http.HandlerFunc(GetDocumentsHandler)))
+	
+
 	// работа и запуск сервера
 	log.Println("запуск сервера http://localhost:8080")
 
