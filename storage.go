d40629661d20b9bd1c1dcda584a415aeb3b7e376
Akram Tsechoev <karabulaki@inbox.ru>
2026-06-20 21:34:14 +0300

    Update storage.go

--- storage.go
+++ storage.go
@@ -2,19 +2,84 @@ package main
 
 var profile = Info{PhoneNumber: "11111111", Email: "i_ivanov@example.ru", Manager: "Ivan Ivanov"}
 
-var orders = []Order{
-	{ID: 40364, Title: "Заказ №1", StageID: "C1:NEW"},
-	{ID: 40365, Title: "Заказ №2", StageID: "C1:WON"},
-	{ID: 40366, Title: "Заказ №3", StageID: "C1:ADD"},
+var ordersDTO = []OrderListItemDTO{
+	{
+		ID: 			   40364,
+	 	Number: 		   "ЗК-2024-0901", 
+		Title: 			   "Насосная станция НС-150",
+		StatusCode: 	   "C1:NEW",
+		StatusGroup: 	   "NEW",
+		StatusLabel: 	   "Новый",
+		Date:		       "2026-05-21",
+		Equipment:		   "Насосная станция НС-150",
+		Quantity:  		    1,
+		QuotationValidTo:  "2026-06-20",
+		InvoiceNumber:	   "СЧ-123",
+		InvoiceValidTo:	   "2026-06-20",
+	},
+
+	{	
+		ID: 			   40365,
+	 	Number: 		   "ЗК-2024-0902", 
+		Title: 			   "Насосная станция НС-151",
+		StatusCode: 	   "C1:NEW",
+		StatusGroup: 	   "NEW",
+		StatusLabel: 	   "Новый",
+		Date:		       "2026-05-21",
+		Equipment:		   "Насосная станция НС-151",
+		Quantity:  		    1,
+		QuotationValidTo:  "2026-06-20",
+		InvoiceNumber:	   "СЧ-124",
+		InvoiceValidTo:	   "2026-06-20",
+
+	},
+
+	{
+		ID: 			   40366,
+	 	Number: 		   "ЗК-2024-0903", 
+		Title: 			   "Насосная станция НС-152",
+		StatusCode: 	   "C1:NEW",
+		StatusGroup: 	   "NEW",
+		StatusLabel: 	   "Новый",
+		Date:		       "2026-05-21",
+		Equipment:		   "Насосная станция НС-152",
+		Quantity:  		    1,
+		QuotationValidTo:  "2026-06-20",
+		InvoiceNumber:	   "СЧ-125",
+		InvoiceValidTo:	   "2026-06-20",
+	},
+	// {ID: 40365, Number: "ЗК-2024-0902", Title: "Заказ №2"},
+	// {ID: 40366, Number: "ЗК-2024-0903", Title: "Заказ №3"},
 }
 
-var documents = []Document{
-	//{ID: 1, Title: "Спецификация", Body: "Документ пока недоступен"},
-
-	{ID: 1, Title: "Спецификация", Body: "В процессе", OrderID: 40366},
+var details = []OrderDetailsDTO{
+	{
+		ID:				40364,
+		Number:	 		"ЗК-2024-0901",
+		Title: 			"Насосная станция НС-150",
+		StatusCode: 	"C1:NEW",
+		StatusGroup: 	"NEW",
+		StatusLabel: 	"Новый",
+		Equipment: 		"Насосная станция НС-150",
+		Quantity: 		1,
+		PaymentPercent: 50,
+		Category: 		"Оборудование",
+		DeliveryType: 	"ТК",
+		DeliveryAddress: "Москва, ул. Ленина",
+		Consignee: 		"Покупатель",
+		Payer: 			"Покупатель",
+		TransportCompany: "СДЭК",
+		TransportWaybill: "ТН-123456",
+		PublicComment: 	  "Нет комментариев",
+},
 }
 
-var login = []LoginRequest{
+var documents = []DocumentDTO{
+
+	{ID: 1, OrderID: 40366, Title: "Спецификация", Type: "Specification", VisibleForClient: true},
+}
+
+var req = []LoginRequest{
 	{Login:"client@example.com", Password:"password"},
 	
 }
