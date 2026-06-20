c672c5b09df8de6314c109b1e17c4728c2ebfb4b
Akram Tsechoev <karabulaki@inbox.ru>
2026-06-20 21:32:38 +0300

    Update models.go

--- models.go
+++ models.go
@@ -1,26 +1,53 @@
 package main
 
-
-type Order struct {
-	ID      int    `json:"id"`
-	Title   string `json:"title"`
-	StageID string `json:"stageId"`
+type OrderListItemDTO struct {
+    ID                   int    `json:"id"`
+    Number               string `json:"number"`
+    Title                string `json:"title"`
+    StatusCode           string `json:"status_code"`
+    StatusGroup          string `json:"status_group"`
+    StatusLabel          string `json:"status_label"`
+    Date                 string `json:"date"`
+    Equipment            string `json:"equipment"`
+    Quantity             int    `json:"quantity"`
+    QuotationValidTo     string `json:"quotation_valid_to"`
+    InvoiceNumber        string `json:"invoice_number"`
+    InvoiceValidTo       string `json:"invoice_valid_to"`
 }
 
+type OrderDetailsDTO struct{
+	ID				 int	 `json:"id"`
+	Number			 string	 `json:"number"`
+	Title			 string  `json:"title"`
+	StatusCode		 string  `json:"status_code"`
+	StatusGroup		 string	 `json:"status_group"`
+	StatusLabel		 string	 `json:"status_label"`
+	Equipment		 string	 `json:"equipment"`
+	Quantity		 int	 `json:"quantity"` 
+	PaymentPercent   int	 `json:"payment_percent"`
+	Category		 string	 `json:"category"`
+	DeliveryType	 string  `json:"Delivery_type"`
+	DeliveryAddress  string	 `json:"Delivery_address"`
+	Consignee		 string	 `json:"consigne"`
+	Payer 			 string  `json:"payer"`
+	TransportCompany string	 `json:"transport_company"`
+	TransportWaybill string  `json:"transport_waybill"`
+	PublicComment    string  `json:"public_comment"`
+}
 
 type Info struct {
-	PhoneNumber string `json:"phone_number"`
-	Email        string `json:"email"`
-	Manager      string `json:"manager"`
+	PhoneNumber 	string `json:"phone_number"`
+	Email       	string `json:"email"`
+	Manager     	string `json:"manager"`
 }
 
-type ErrorResp struct{
-	Code	string `json:"code"`
+type ErrorResp struct {
+	Code    string `json:"code"`
 	Message string `json:"message"`
 }
 
-type SuccessResp struct{
-	Code	string `json:"code"`
+type SuccessResp struct {
+	Code    string `json:"code"`
 	Message string `json:"message"`
 }
 
@@ -28,31 +55,24 @@ type Comment struct {
 	Text string `json:"text"`
 }
 
-
-type Document struct {
-	ID    int 		`json:"id"`
-	Title string	`json:"title"`
-	Body  string	`json:"body"`
-	OrderID int     `json:"order_id"`
+type DocumentDTO struct {
+	ID      int    `json:"id"`
+	OrderID int    `json:"order_id"`
+	Title   string `json:"title"`
+	Type 	string `json:"type"`
+	VisibleForClient bool `json:"visible_for_client"`
 }
-
-
-
-type Comment struct{
-	Text	string `json:"text"`
-
-	}
 
 type Auth struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
 }
 
-
 type LoginRequest struct {
-	Login    string `json:"login"`
-	Password string `json:"password"`
+	Login   	string		 `json:"login"`
+	Password 	string       `json:"password"`
 }
 
-
-
+type LogResp struct {
+	Token string `json:"token"`
+}
