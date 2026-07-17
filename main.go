06f509c1dd5fe8b67ea3fbe24b058be283c56005
Akram Tsechoev <karabulaki@inbox.ru>
2026-07-17 18:31:19 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,37 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-
-	
 )
 
 func main() {
-	
-	db, err := InitDB()
-	if err != nil{
-		log.Fatal("Нет подключения к БД", err)
-	}
-	defer db.Close()
-
-
-	
-	http.HandleFunc("/api/cabinet/auth/login", PostLoginHandler)
-	http.HandleFunc("/api/cabinet/auth/logout", PostLogoutHandler)
-	http.HandleFunc("/api/cabinet/auth/me", GetAuthHandler)
-
-	// обертка handle
-	http.Handle("/api/cabinet/orders", AuthMiddleware(http.HandlerFunc(GetOrderHandler)))
-	http.Handle("/api/cabinet/orders/{id}", AuthMiddleware(http.HandlerFunc(GetIdHandler)))
-	http.Handle("/api/cabinet/orders/{id}/comment", AuthMiddleware(http.HandlerFunc(PostCommentHandler)))
-	http.Handle("/api/cabinet/profile", AuthMiddleware(http.HandlerFunc(GetProfileHandler)))
-	http.Handle("/api/cabinet/orders/{id}/documents", AuthMiddleware(http.HandlerFunc(GetDocumentsHandler)))
-	
 
 	// работа и запуск сервера
 	log.Println("запуск сервера http://localhost:8080")
 
 	// запуск по порталу
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", newRouter()))
 }
-
-	
