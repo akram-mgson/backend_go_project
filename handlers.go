16b2461a42fca2ebd83410fed38110cd49da7c8a
Akram Tsechoev <karabulaki@inbox.ru>
2026-07-17 18:35:37 +0300

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -6,179 +6,187 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"fmt"
 )
 
-func GetOrderHandler(w http.ResponseWriter, r *http.Request){
+func GetOrderHandler(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method != http.MethodGet{
-		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
-			return
-		}
-		writeJSON(w, http.StatusOK, ordersDTO)
-		return
-
-	}
-
-func GetIdHandler(w http.ResponseWriter, r *http.Request){
-		id := r.PathValue("id") // вытаскиваем id из пути/конкретный заказ клиента
-		if r.Method != http.MethodGet{
-			writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается" )
-			// отправка ошибки 405
-			return 
-		}
-
-		fmt.Println("DEBUG: r.URL.Path =", r.URL.Path)
-		fmt.Println("DEBUG: r.PathValue('id') =", r.PathValue("id"))
-		
-
-		idInt, err := strconv.Atoi(id)
-		if err != nil {
-			
-			writeError(w, http.StatusBadRequest, "INVALID_REQUEST", "Неверный запрос")
-			return
-		}
-	
-		
-
-		for _, current := range details {
-			if current.ID == idInt {
-
-				writeJSON(w, http.StatusOK, current)
-				return
-			}
-		}
-		writeError(w, http.StatusNotFound, "ORDER_NOT_FOUND", "Заказ не найден")
-			return 
-	}
-
-func PostLoginHandler(w http.ResponseWriter, r *http.Request){	
-	if r.Method != http.MethodPost{
+	if r.Method != http.MethodGet {
 		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
 		return
-		}
+	}
+	writeJSON(w, http.StatusOK, ordersDTO)
+	return
 
-		var req LoginRequest
-		
-		err := json.NewDecoder(r.Body).Decode(&req)
-		if err != nil{ 
-			writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Неправильный запрос")
+}
+
+func GetIdHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id") // вытаскиваем id из пути/конкретный заказ клиента
+	if r.Method != http.MethodGet {
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
+		return
+	}
+
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+
+		writeError(w, http.StatusBadRequest, "INVALID_REQUEST", "Неверный запрос")
+		return
+	}
+
+
+	for _, current := range details {
+		if current.ID == idInt {
+			writeJSON(w, http.StatusOK, current)
 			return
 		}
+	}
+	writeError(w, http.StatusNotFound, "ORDER_NOT_FOUND", "Заказ не найден")
+	return
+}
 
-		if req.Login != "client@example.com" || req.Password != "password"{
+func PostLoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
+		return
+	}
+
+	var req LoginRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Неправильный запрос")
+		return
+	}
+
+	if req.Login == "" || req.Password == "" {
+		writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "логин и пароль обязательны")
+		return
+	}
+
+	if req.Login != "client@example.com" || req.Password != "password" {
 		writeError(w, http.StatusUnauthorized, "INVALID_CREDENTIALS", "Неправильный логин или пароль")
 		return
-		}
-		writeJSON(w, http.StatusOK, map[string]string{"token": "fake-token"})
 	}
 
+	writeJSON(w, http.StatusOK, map[string]string{"token": "fake-token"})
+}
 
-func PostCommentHandler(w http.ResponseWriter, r *http.Request){
+func PostCommentHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	
-		if r.Method != http.MethodPost {
+
+	if r.Method != http.MethodPost {
 		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
 		return
 	}
 	idInt2, err := strconv.Atoi(id)
 
-	if err != nil{
-			writeError(w, http.StatusBadRequest, "INVALID_REQUEST", "Некорректный запрос")
-			return
+	if err != nil {
+		writeError(w, http.StatusBadRequest, "INVALID_REQUEST", "Некорректный запрос")
+		return
 	}
 
 	var found bool
-	for _, new_current := range ordersDTO{
-		if new_current.ID == idInt2{
+	for _, new_current := range ordersDTO {
+		if new_current.ID == idInt2 {
 			found = true
 			break
 		}
 	}
-	if found == false{
+	if found == false {
 		writeError(w, http.StatusNotFound, "ORDER_NOT_FOUND", "Заказ не найден")
 		return
 	}
 
-		var comment Comment
-		defer r.Body.Close()
-		bodyBytes, err := io.ReadAll(r.Body)
-		err = json.Unmarshal(bodyBytes, &comment) 
-		if err != nil{
-			writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Некорректный JSON")
-			return
-		}
-		if strings.TrimSpace(comment.Text) == ""{
-			writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Комментарий не должен быть пустым")
-			return
-		}
-		writeJSON(w, http.StatusOK, map[string]string{"message": "Комментарий отправлен. Данные обновятся после синхронизации."})
-}
+	var comment Comment
+	defer r.Body.Close()
+	bodyBytes, err := io.ReadAll(r.Body)
 
-
-func GetProfileHandler(w http.ResponseWriter, r *http.Request){	
-	if r.Method != http.MethodGet{
-		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
+	
+	err = json.Unmarshal(bodyBytes, &comment) 
+	if err != nil {
+		writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Некорректный JSON")
 		return
-		}
-		writeJSON(w, http.StatusOK, profile)
 	}
 
-func GetDocumentsHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodGet{
+	
+	if strings.TrimSpace(comment.Text) == "" {
+		writeError(w, http.StatusBadRequest, "VALIDATION_ERROR", "Комментарий не должен быть пустым")
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]string{"message": "Комментарий отправлен. Данные обновятся после синхронизации."})
+}
+
+func GetProfileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
+		return
+	}
+	writeJSON(w, http.StatusOK, profile)
+}
+
+func GetDocumentsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
 		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
 		return
 	}
 	id := r.PathValue("id")
 	id2, err := strconv.Atoi(id)
-	if err != nil{
-	
+	if err != nil {
+
 		writeError(w, http.StatusBadRequest, "INVALID_REQUEST", "Неверный запрос")
 		return
 	}
-		
-		var res bool
-		for _, Order := range ordersDTO{
-			
-			
-				if Order.ID == id2{
-					res = true
-					break
-				} 
-			}
-			if res == false {
-				writeError(w, http.StatusNotFound, "ORDER_NOT_FOUND", "Заказ не найден")
-				return 
-			}
+
+
+	var res bool
+	for _, Order := range ordersDTO {
+
+		if Order.ID == id2 {
+			res = true
+			break
+		}
+	}
+	if res == false {
+		writeError(w, http.StatusNotFound, "ORDER_NOT_FOUND", "Заказ не найден")
+		return
+	}
 
 	
-			filtered := make([]DocumentDTO, 0)
-			for _, doc := range documents{
-				if doc.OrderID == id2{
-					filtered = append(filtered, doc)
-				}
-			}
-			writeJSON(w, http.StatusOK, filtered)
+	filtered := make([]DocumentDTO, 0)
+	for _, doc := range documents {
+		if doc.OrderID == id2 && doc.VisibleForClient == true {
+			filtered = append(filtered, doc)
+		}
+	}
+
+	for _, doc := range documents {
+		if doc.OrderID == id2 && !doc.VisibleForClient {
+			writeError(w, http.StatusForbidden, "FORBIDDEN", "Документы скрыты")
 			return
-			
-}
-func PostLogoutHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodPost{
-	writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
+		}
+	}
+
+	writeJSON(w, http.StatusOK, filtered)
 	return
-}
-	writeJSON(w, http.StatusOK, map[string]string{"text": "loggout"})
 
 }
 
-func GetAuthHandler(w http.ResponseWriter, r *http.Request){
-	
-
-	if r.Method != http.MethodGet{
+func PostLogoutHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
 		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
 		return
 	}
-	
+	writeJSON(w, http.StatusOK, map[string]string{"text": "logged out"})
+
+}
+
+func GetAuthHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		writeError(w, http.StatusMethodNotAllowed, "METHOD_NOT_ALLOWED", "Метод не поддерживается")
+		return
+	}
+	// создал переменную в обработчике
 	user := Auth{Name: "Ivan", Email: "i_ivanov@test.com"}
 	writeJSON(w, http.StatusOK, user)
 }
